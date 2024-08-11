c5c191451c14d8c093eb22b743ee400b0858cd42
bigsky77 <simonjudd2@gmail.com>
2024-08-11 07:37:10 -0600

    refactor

--- src/main.rs
+++ src/main.rs
@@ -4,53 +4,99 @@ extern crate nalgebra as na;
 extern crate rand;
 extern crate num_bigint;
 extern crate num_traits;
+extern crate ronkathon;
 
-mod polynomial;
-mod prover;
-mod verifier;
+use std::collections::BTreeMap;
 
-use prover::prove;
-use verifier::verify;
+use ronkathon::{polynomial::multivariate_polynomial::MultivariatePolynomial, algebra::field::prime::PlutoBaseField, algebra::field::prime::PlutoPrime, algebra::field::prime::PrimeField};
+use ronkathon::polynomial::sumcheck::non_interactive_sumcheck_prove;
+use ronkathon::random::Random;
 
-use polynomial::{FiniteField, PolynomialRing};
-use na::DVector;
+use rand::{Rng, random};
+use na::{DVector, DMatrix};
+
+lazy_static! {
+    pub static ref M1: DMatrix<PlutoBaseField> = DMatrix::from_row_slice(4, 8, &[
+        PlutoBaseField::from(1), PlutoBaseField::from(1), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0),
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(1), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0),
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(1), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0),
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0),
+   ]);
+
+    pub static ref M2: DMatrix<PlutoBaseField> = DMatrix::from_row_slice(4, 8, &[
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(1),
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(1), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0),
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(1), PlutoBaseField::from(0), PlutoBaseField::from(0),
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0),
+   ]);
+
+    pub static ref M3: DMatrix<PlutoBaseField> = DMatrix::from_row_slice(4, 8, &[
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(1), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0),
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(1), PlutoBaseField::from(0), PlutoBaseField::from(0),
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(1), PlutoBaseField::from(0),
+        PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0), PlutoBaseField::from(0),
+   ]);
+}
+
+impl Random for PlutoBaseField {
+  fn random<R: Rng + ?Sized>(rng: &mut R) -> Self {
+    let value = rng.gen_range(0..PlutoPrime::Base as usize);
+    PlutoBaseField::new(value)
+  }
+}
 
 fn main() {
 
     println!();
     println!("\x1b[32mSparta(0) Starting Execution\x1b[0m");
 
-    // Finite field prime
-    let prime = 7;
-
     // Variable names as a single string
     let variable_names = "x1, x2, y1, y2, y3, x11, x22, y11, y22, y33";
 
-    // Create PolynomialRing
-    let ring = PolynomialRing::new(prime, 10, variable_names);
+    let poly = MultivariatePolynomial::<PlutoBaseField>::from_terms(vec![
+        MultivariateTerm::new(
+        vec![MultivariateVariable { index: 1, exponent: 1 }],
+        PlutoBaseField::new(1)
+        ),
+        MultivariateTerm::new(
+        vec![MultivariateVariable { index: 2, exponent: 1 }],
+        PlutoBaseField::new(1)
+        ),
+        MultivariateTerm::new(
+        vec![MultivariateVariable { index: 3, exponent: 1 }],
+        PlutoBaseField::new(2)
+        ),
+        MultivariateTerm::new(
+        vec![MultivariateVariable { index: 4, exponent: 1 }],
+        PlutoBaseField::new(3)
+        ),
+        MultivariateTerm::new(
+        vec![MultivariateVariable { index: 5, exponent: 1 }],
+        PlutoBaseField::new(6)
+        ),
+        MultivariateTerm::new(
+        vec![MultivariateVariable { index: 6, exponent: 1 }],
+        PlutoBaseField::new(6)
+        ),
+        MultivariateTerm::new(
+        vec![MultivariateVariable { index: 0, exponent: 1 }],
+        PlutoBaseField::new(1)
+        )
+    ]);
 
-    // Generate variables
-    let _vars = ring.gens();
+    println!("Z1 Polynomial: {:?}", poly.to_string());
+    //let mz1 = poly.mul_matrix(&M1);
+    //let mz2 = poly.mul_matrix(&M1);
+    //let mz3 = poly.mul_matrix(&M1);
 
-    let x = vec![0, 1, 1, 2, 3, 6, 6];
-    let w: Vec<i32> = Vec::new();
+    //let elementwise_product = mz1.mul;
+    //let result = elementwise_product;
 
-    // set values to finite field
-    let combined: Vec<i32> = x.iter().chain(&w).cloned().collect();
-    let combined_ff: Vec<FiniteField> = combined.iter().map(|&v| FiniteField::new(v, prime)).collect();
+    //// Run the prover
+    non_interactive_sumcheck_prove::<PlutoBaseField>(&poly);
 
-    let mut extended_combined_ff = combined_ff.clone();
-    extended_combined_ff.push(FiniteField::new(1, prime));
-
-    let combined_values: Vec<i32> = extended_combined_ff.iter().map(|ff| ff.get_value()).collect();
-    let z1 = DVector::from_vec(combined_values);
-    println!("      Z1: {:?}", z1);
-
-    // Run the prover
-    let proof = prove(z1);
-
-    // Run the verifier
-    verify(proof);
+    //// Run the verifier
+    //verify(proof);
 
     println!();
     println!("\x1b[32mSparta(0) Execution Complete\x1b[0m");
