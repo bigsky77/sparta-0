577d02269fdbb614e9e0d0e65c459852df412618
bigsky77 <simonjudd2@gmail.com>
2024-08-11 08:40:05 -0600

    CCS -> Prover -> Verifier compiles and runs

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ extern crate num_traits;
 extern crate ronkathon;
 
 use std::collections::BTreeMap;
+use std::ops::{Mul, Sub};
 
+use ronkathon::polynomial::multivariate_polynomial::{MultivariateTerm, MultivariateVariable};
 use ronkathon::{polynomial::multivariate_polynomial::MultivariatePolynomial, algebra::field::prime::PlutoBaseField, algebra::field::prime::PlutoPrime, algebra::field::prime::PrimeField};
-use ronkathon::polynomial::sumcheck::non_interactive_sumcheck_prove;
+use ronkathon::sumcheck::{non_interactive_sumcheck_prove, non_interactive_sumcheck_verify};
 use ronkathon::random::Random;
 
 use rand::{Rng, random};
@@ -38,66 +40,61 @@ lazy_static! {
    ]);
 }
 
-impl Random for PlutoBaseField {
-  fn random<R: Rng + ?Sized>(rng: &mut R) -> Self {
-    let value = rng.gen_range(0..PlutoPrime::Base as usize);
-    PlutoBaseField::new(value)
-  }
-}
-
 fn main() {
 
     println!();
-    println!("\x1b[32mSparta(0) Starting Execution\x1b[0m");
-
-    // Variable names as a single string
-    let variable_names = "x1, x2, y1, y2, y3, x11, x22, y11, y22, y33";
+    println!("\x1b[32mSparta(0): Starting Execution\x1b[0m");
 
     let poly = MultivariatePolynomial::<PlutoBaseField>::from_terms(vec![
-        MultivariateTerm::new(
-        vec![MultivariateVariable { index: 1, exponent: 1 }],
-        PlutoBaseField::new(1)
-        ),
-        MultivariateTerm::new(
-        vec![MultivariateVariable { index: 2, exponent: 1 }],
-        PlutoBaseField::new(1)
-        ),
-        MultivariateTerm::new(
-        vec![MultivariateVariable { index: 3, exponent: 1 }],
-        PlutoBaseField::new(2)
-        ),
-        MultivariateTerm::new(
-        vec![MultivariateVariable { index: 4, exponent: 1 }],
-        PlutoBaseField::new(3)
-        ),
-        MultivariateTerm::new(
-        vec![MultivariateVariable { index: 5, exponent: 1 }],
-        PlutoBaseField::new(6)
-        ),
-        MultivariateTerm::new(
-        vec![MultivariateVariable { index: 6, exponent: 1 }],
-        PlutoBaseField::new(6)
-        ),
-        MultivariateTerm::new(
-        vec![MultivariateVariable { index: 0, exponent: 1 }],
-        PlutoBaseField::new(1)
-        )
-    ]);
+            MultivariateTerm::new(
+            vec![MultivariateVariable { index: 1, exponent: 1 }],
+            PlutoBaseField::new(1)
+            ),
+            MultivariateTerm::new(
+            vec![MultivariateVariable { index: 2, exponent: 1 }],
+            PlutoBaseField::new(1)
+            ),
+            MultivariateTerm::new(
+            vec![MultivariateVariable { index: 3, exponent: 1 }],
+            PlutoBaseField::new(2)
+            ),
+            MultivariateTerm::new(
+            vec![MultivariateVariable { index: 4, exponent: 1 }],
+            PlutoBaseField::new(3)
+            ),
+            MultivariateTerm::new(
+            vec![MultivariateVariable { index: 5, exponent: 1 }],
+            PlutoBaseField::new(6)
+            ),
+            MultivariateTerm::new(
+            vec![MultivariateVariable { index: 6, exponent: 1 }],
+            PlutoBaseField::new(6)
+            ),
+            MultivariateTerm::new(
+            vec![MultivariateVariable { index: 0, exponent: 1 }],
+            PlutoBaseField::new(1)
+            )
+        ]);
 
     println!("Z1 Polynomial: {:?}", poly.to_string());
-    //let mz1 = poly.mul_matrix(&M1);
-    //let mz2 = poly.mul_matrix(&M1);
-    //let mz3 = poly.mul_matrix(&M1);
 
-    //let elementwise_product = mz1.mul;
-    //let result = elementwise_product;
+    let mz1 = &poly.clone().mul_matrix(&M1);
+    let mz2 = &poly.clone().mul_matrix(&M2);
+    let mz3 = &poly.clone().mul_matrix(&M3);
+
+    let elementwise_product = &mz1.clone().mul(mz2.clone());
+    let z_poly = elementwise_product.clone().sub(mz3.clone());
 
     //// Run the prover
-    non_interactive_sumcheck_prove::<PlutoBaseField>(&poly);
+    println!();
+    println!("\x1b[32mSparta(0): Running Prover\x1b[0m");
+    let proof = non_interactive_sumcheck_prove::<PlutoBaseField>(&z_poly);
 
     //// Run the verifier
-    //verify(proof);
+    println!();
+    println!("\x1b[32mSparta(0): Running Verifier\x1b[0m");
+    non_interactive_sumcheck_verify(&proof, &z_poly);
 
     println!();
-    println!("\x1b[32mSparta(0) Execution Complete\x1b[0m");
+    println!("\x1b[32mSparta(0): Execution Complete\x1b[0m");
 }
